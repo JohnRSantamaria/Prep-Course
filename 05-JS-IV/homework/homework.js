ff1357bda250fa6eca6d7fc40993883c344c3bd2
John Santamaria <santamariajohnd@gmail.com>
2022-07-23 22:51:37 -0500

    Ejercicios 5

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,18 +1,28 @@
 // No cambies los nombres de las funciones.
 
-function crearGato (nombre, edad) {
+function crearGato (nombre, edad) { //Done! 
   // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var nuevoGato = {
+    nombre: nombre,
+    edad: edad,
+    meow(){
+      return "Meow!";
+    }
+  }
+  return nuevoGato;
 }
 
-function agregarPropiedad (objeto, property) {
+function agregarPropiedad (objeto, property) { 
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
+    
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,33 +30,46 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+
 }
 
-function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
+function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) { //Done
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  return objetoMisterioso.numeroMisterioso*=5;
 }
 
-function eliminarPropiedad (objeto, unaPropiedad) {
+function eliminarPropiedad (objeto, unaPropiedad) {  //done
   // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto [unaPropiedad]
+  return(objeto);
 }
 
-function nuevoUsuario (nombre, email, password) {
+function nuevoUsuario (nombre, email, password) { //done
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var newObj ={
+    nombre: nombre,
+    email: email,
+    password : password,
+  }
+  return(newObj);
 }
 
-function tieneEmail (usuario) {
+function tieneEmail (usuario) { //done
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario.email){
+    return true;
+  }{
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -54,6 +77,12 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(objeto["propiedad"]==propiedad){
+     return true;
+  }else{
+    return false;
+  }
+
 }
 
 function verificarPassword (usuario, password) {
@@ -61,19 +90,37 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(password.lengh === usuario.password.lengh){  //Done
+    if(password == usuario.password){
+      return true
+    }else{
+      return false
+    }
+
+  }else{
+    return false
+  }
 }
 
-function actualizarPassword (usuario, nuevaPassword) {
-  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
+function actualizarPassword (usuario, nuevaPassword) { //done
+  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+     usuario.password = nuevaPassword
+
+     return(usuario);
+
 }
 
-function agregarAmigo (usuario, nuevoAmigo) {
+function agregarAmigo (usuario, nuevoAmigo) { //Done
   // "usuario" tiene una propiedad llamada "amigos" que es un array
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+
+  usuario.amigos.push(nuevoAmigo)
+
+  return(usuario);
 }
 
 function pasarUsuarioAPremium (usuarios) {
@@ -82,8 +129,10 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-}
 
+
+
+}
 function sumarLikesDeUsuario (usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
@@ -91,6 +140,8 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  usuario [posts][post][likes] =+1;
+  return(usuario);
 }
 
 function agregarMetodoCalculoDescuento (producto) {
